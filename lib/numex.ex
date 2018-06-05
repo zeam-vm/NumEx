8b6bd9e8d93c24a9b0bccb7f0bfc424280104867
yukq16 <shikine.yui@gmail.com>
2018-06-05 16:03:03 +0900

    first commit

---
+++ lib/numex.ex
@@ -0,0 +1,18 @@
+defmodule Numex do
+  @moduledoc """
+  Documentation for Numex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Numex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/numex_test.exs
@@ -0,0 +1,8 @@
+defmodule NumexTest do
+  use ExUnit.Case
+  doctest Numex
+
+  test "greets the world" do
+    assert Numex.hello() == :world
+  end
+end
