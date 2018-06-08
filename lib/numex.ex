f55a2c8e8aa9bc9aa71e76d1f9942f7c9e320dfe
yukq16 <shikine.yui@gmail.com>
2018-06-08 20:33:35 +0900

    Added Function add(), prd()

--- lib/numex.ex
+++ lib/numex.ex
@@ -1,4 +1,5 @@
 defmodule Numex do
+  defstruct list: []
   @moduledoc """
   Documentation for Numex.
   """
@@ -15,4 +16,37 @@ defmodule Numex do
   def hello do
     :world
   end
+
+  """
+  def zeros(list, num) do
+    if num != 0 do
+      list ++ [zeros(list, num-1)]
+    else
+
+    end
+  end
+  """
+  # 要素ごとの加算
+  def add([a|tail1], [b|tail2]) do
+    [a+b|add(tail1, tail2)] 
+  end
+
+  def add([], []), do: []
+  
+  # 要素ごとの乗算
+  # Pointwise product
+  def prd([a|tail1], [b|tail2]) do
+    [a*b|prd(tail1, tail2)]
+  end
+
+  def prd([], []), do: []
+  
+  def dot([a|tail1],[b|tail2]) do
+      
+  end
+
+  def dot(num, [], []) , do: []
 end
+
+#Numex.add(,[1, 2, 3], [2, 3, 4])
+#Numex.prd([1, 2, 3], [4, 5, 6])
--- test/numex_test.exs
+++ test/numex_test.exs
@@ -5,4 +5,12 @@ defmodule NumexTest do
   test "greets the world" do
     assert Numex.hello() == :world
   end
+
+  test "add like numpy" do
+    assert Numex.add([1, 1], [2, 3]) == [3, 4]
+  end
+
+  test "prd test" do
+    assert Numex.prd([1,2,3],[1,2,3]) == [1, 4, 9]
+  end
 end
